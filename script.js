9fe9f487570767cd7876ed382178b66614c31ca0
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-20 20:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-function fibonacci(c) {
-	 // let cc1 = prompt();
-  //       let c1 = parseInt(cc1);
-  //       fibonacci(c1); 
-  //       function fibonacci(c) {
-            let c1 = 0;
-            let f1 = 0;
-            let f2 = 1;
-            var sum=0;
-            if (c == 1) {
-                console.log(f1);
-                return;
-            } else if (c == 2) {
-                console.log(f2);
-                return;
-            }
-            c1 = 3;
-            while (c1 <= c) {
-                sum=f1+f2;
-                f1=f2;
-                f2=sum;
-                c1++
-
-            }
-            console.log(sum);
+function fibonacci(num) {
+let a = 0;
+let b = 1;
+let temp = 0;
+for(let i = 1; i<num; i++){
+	temp = a; 
+	a = a+b;
+	b = temp;
 	
-
 }
+	return a; 
 	
+	
+}
 
 module.exports = fibonacci;
